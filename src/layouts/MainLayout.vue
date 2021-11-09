ed995e6b2fe14c9e870f64a362403e8b7e3a23b4
Yifan - Macbook Pro <zyfinori@outlook.com>
2021-11-08 21:32:57 -0500

    fixed login and register route, added dependencies to package.json

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -59,10 +59,10 @@ export default {
     },
     methods: {
         onLoginButtonClick() {
-            this.$router.push("users/login")
+            this.$router.push("/users/login")
         },
         onRegisterButtonClick() {
-            this.$router.push("users/register")
+            this.$router.push("/users/register")
         }
     }
 }
